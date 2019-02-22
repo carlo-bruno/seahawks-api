e936284d282518263a1c894d2142d9f482926151
Carlo Bruno <carlofbruno@gmail.com>
2019-02-21 17:32:39 -0800

    added sequelize, created initial seeder file

---
+++ models/player.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const player = sequelize.define('player', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    number: DataTypes.INTEGER,
+    position: DataTypes.STRING
+  }, {});
+  player.associate = function(models) {
+    // associations can be defined here
+  };
+  return player;
+};
---
+++ seeders/20190222011925-roster02-19.js
@@ -0,0 +1,62 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+
+    return queryInterface.bulkInsert("games", [
+      {
+        firstName: "Russel",
+        lastName: "Wilson",
+        number: 3,
+        position: "QB",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: "Doug",
+        lastName: "Baldwin",
+        number: 89,
+        position: "WR",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: "Tyler",
+        lastName: "Lockett",
+        number: 16,
+        position: "WR",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        firstName: "Bobby",
+        lastName: "Wagner",
+        number: 54,
+        position: "LB",
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+    return queryInterface.bulkDelete("players", null, {});
+  }
+};
